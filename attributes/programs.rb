efa3dc774e0babd8ba61c8c4d8424ce2ffbfa432
212050466 <josh.schneider@ge.com>
2017-03-10 10:51:32 -0600

    changed order of deploy

--- attributes/programs.rb
+++ attributes/programs.rb
@@ -1,1 +1,1 @@
-default['winbox']['additional_apps'] = %w(launchy notepadplusplus vagrant virtualbox p4merge p4v p4 7zip putty javaruntime)
+default['winbox']['additional_apps'] = %w(launchy notepadplusplus p4merge p4v p4 7zip putty javaruntime vagrant virtualbox)
