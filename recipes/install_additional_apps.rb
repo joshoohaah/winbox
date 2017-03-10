c0104a008880814e76ce5594aa012462c8d8d79d
212050466 <josh.schneider@ge.com>
2017-03-10 11:00:14 -0600

    try with powershell block

--- recipes/install_additional_apps.rb
+++ recipes/install_additional_apps.rb
@@ -1,7 +1,15 @@
 node['winbox']['additional_apps'].each do |pack|
-  chocolatey_package pack do
-    options '--allow-empty-checksums --ignore-checksums'
-    returns [0, 3010]
-    action :install
-  end
+#   chocolatey_package pack do
+#     options '--allow-empty-checksums --ignore-checksums'
+#     returns [0, 3010]
+#     action :install
+#   end
+# end
+powershell_script 'install apps' do
+  cwd '%TEMP%'
+  code <<-EOH
+  chocolatey install #{pack} -y
+  EOH
+  returns [0, 3010]
+end
 end
