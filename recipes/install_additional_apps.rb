12ed89bab9dde37bf0afb56b956eaf7f05027298
212050466 <josh.schneider@ge.com>
2017-03-10 10:52:37 -0600

    added in acceptable return codes

--- recipes/install_additional_apps.rb
+++ recipes/install_additional_apps.rb
@@ -1,6 +1,7 @@
 node['winbox']['additional_apps'].each do |pack|
   chocolatey_package pack do
     options '--allow-empty-checksums --ignore-checksums'
+    returns [0, 3010]
     action :install
   end
 end
