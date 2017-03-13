28a7f61b36abfb846bf04ebb2f723f11f8d2ff69
212050466 <josh.schneider@ge.com>
2017-03-13 12:12:59 -0500

    adding in flowdock

--- attributes/programs.rb
+++ attributes/programs.rb
@@ -1,1 +1,1 @@
-default['winbox']['additional_apps'] = %w(launchy notepadplusplus github p4merge p4v p4 7zip putty javaruntime vagrant virtualbox)
+default['winbox']['additional_apps'] = %w(launchy notepadplusplus flowdock github p4merge p4v p4 7zip putty javaruntime vagrant virtualbox)
